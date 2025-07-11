4701bec51a7895607016fbba29bab79a85817a8c
Shivanandkoli09 <kolishivanand4@gmail.com>
2025-07-11 13:34:38 +0530

    Initial Commit

---
+++ WeatherAlert/WeatherAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAlertApp.swift
+//  WeatherAlert
+//
+//  Created by KPIT on 11/07/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
